777dac09cd313548c788a4c5e1db98ac84557b29
Cameron Potts <cgp7974@gmail.com>
2018-11-25 14:55:02 -0800

    test path7

--- home_api.py
+++ home_api.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>This is a test page from Flask Python</h1>"
 
-@app.route("testpath/")
-def hello2():
+@app.route("/testpath")
+def hello():
     return "<h1 style='color:blue'>This message is from the test path</h1>"
 
 if __name__ == "__main__":
